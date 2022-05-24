dfa66d32c7101211303b1dfe59ee89a247207fc4
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-24 19:42:00 +0800

    Adds active state to arrow buttons and updates FIXMEs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // TODO: make responsive for mobile devices
 
 // FIXME: bug when moving between expandable menu items very quickly (ie, under 200ms)
+// FIXME: fix gap at bottom of img container
 
 const expandableMenus = document.querySelectorAll('.expandable');
 expandableMenus.forEach((menu) => {
