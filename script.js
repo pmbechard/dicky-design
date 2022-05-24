b976a875437c2e724223ea9d2420340476c250d0
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-24 19:39:07 +0800

    Styles arrows and adds TODOs and FIXMEs to script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// TODO: add picture scrolling functionality and auto-scroll
+// TODO: make responsive for mobile devices
+
+// FIXME: bug when moving between expandable menu items very quickly (ie, under 200ms)
+
 const expandableMenus = document.querySelectorAll('.expandable');
 expandableMenus.forEach((menu) => {
   menu.style.cursor = 'auto';
   menu.addEventListener('mouseenter', () => {
+    expandableMenus.forEach((item) => {
+      item.firstElementChild.style.display = 'none';
+    });
     window.setTimeout(
       () => (menu.firstElementChild.style.display = 'block'),
       200
     );
   });
   menu.addEventListener('mouseleave', () => {
+    expandableMenus.forEach((item) => {
+      item.firstElementChild.style.display = 'none';
+    });
     menu.firstElementChild.style.display = 'none';
   });
 });
