e559941e3d4fdd06d01d0416f3d10f51cd3583fa
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-24 18:54:48 +0800

    Adds arrows to img area and fixes transition bug in navbar

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const expandableMenus = document.querySelectorAll('.expandable');
 expandableMenus.forEach((menu) => {
   menu.style.cursor = 'auto';
-  menu.addEventListener(
-    'mouseover',
-    () => (menu.firstElementChild.style.display = 'block')
-  );
-  menu.addEventListener(
-    'mouseleave',
-    () => (menu.firstElementChild.style.display = 'none')
-  );
+  menu.addEventListener('mouseenter', () => {
+    window.setTimeout(
+      () => (menu.firstElementChild.style.display = 'block'),
+      200
+    );
+  });
+  menu.addEventListener('mouseleave', () => {
+    menu.firstElementChild.style.display = 'none';
+  });
 });
