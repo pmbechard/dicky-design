cddd860f263e19d35514fd3eb7eb461f2b9bab9d
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-25 12:42:14 +0800

    Gives functionality to arrow buttons on img slider

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 // FIXME: bug when moving between expandable menu items very quickly (ie, under 200ms)
 
+// EXPANDABLE MENUS
 const expandableMenus = document.querySelectorAll('.expandable');
 expandableMenus.forEach((menu) => {
   menu.style.cursor = 'auto';
@@ -21,4 +22,28 @@ expandableMenus.forEach((menu) => {
     });
     menu.firstElementChild.style.display = 'none';
   });
+});
+
+// IMAGE SLIDER
+let currentImgNum = 1;
+const currentImg = document.getElementById('displayed-img');
+
+const rightArrow = document.getElementById('right-arrow');
+rightArrow.addEventListener('click', () => {
+  if (currentImgNum === 4) {
+    currentImgNum = 1;
+  } else {
+    currentImgNum++;
+  }
+  currentImg.setAttribute('src', `img/slider/${currentImgNum}.jpg`);
+});
+
+const leftArrow = document.getElementById('left-arrow');
+leftArrow.addEventListener('click', () => {
+  if (currentImgNum === 1) {
+    currentImgNum = 4;
+  } else {
+    currentImgNum--;
+  }
+  currentImg.setAttribute('src', `img/slider/${currentImgNum}.jpg`);
 });
