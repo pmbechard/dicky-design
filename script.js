20189bd9e81e3790961c9bc5f3ddbc9821cd2ccb
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-25 12:29:19 +0800

    Fixes gap at bottom of image container

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // TODO: make responsive for mobile devices
 
 // FIXME: bug when moving between expandable menu items very quickly (ie, under 200ms)
-// FIXME: fix gap at bottom of img container
 
 const expandableMenus = document.querySelectorAll('.expandable');
 expandableMenus.forEach((menu) => {
